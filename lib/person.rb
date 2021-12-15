8498bebc1d0ef61466fab343bea277a7a0a61d99
Noa Udler <udlernoa4@gmail.com>
2021-12-14 17:43:06 -0800

    commit tree 809686e0b7db4f25154eb5f1c14e9c9ed144f450

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
+require 'pry'
 class Person
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def name
+        @name
+    end
+    def job
+        @job
+    end
 
 end
+
+me = Person.new
+me.name = "Noa"
+me.job = "SE"
+
+binding.pry
+
+
+
